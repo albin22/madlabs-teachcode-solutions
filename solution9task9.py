c82924ff53f57e8bd9b6088cbf7c6ddca0805341
albin22 <albinthankachan1@gmail.com>
2017-10-04 12:19:39 +0000

    9Task Submitted

---
+++ solution9task9.py
@@ -0,0 +1,6 @@
+stringVariable = "Play a game with me if you're a Hacker"
+print(stringVariable[32]) 
+print(stringVariable[20: ]) 
+print(stringVariable[0 : 11]) 
+print(stringVariable + " You Just Concatinated ! Hacker !")
+print(stringVariable * 3)                            
