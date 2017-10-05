cbe35d86aa783d71639cd1155cddc58c739093dd
albin22 <albinthankachan1@gmail.com>
2017-10-05 15:24:35 +0000

    15Task Submitted

---
+++ solution15task15.py
@@ -0,0 +1,12 @@
+varOne = True 
+vartwo = False 
+if varOne or vartwo:
+    print("You Got it Right") 
+else:
+    print("I guess you need to tweak more") 
+varOne = True 
+vartwo = True 
+if varOne and vartwo:
+    print("You just made it") 
+else:
+    print("Try hard")
