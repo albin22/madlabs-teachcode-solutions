3e94815a1362b0a99101856bd41612ac69327a4f
albin22 <albinthankachan1@gmail.com>
2017-10-05 15:14:20 +0000

    14Task Submitted

---
+++ solution14task14.py
@@ -0,0 +1,14 @@
+numOne=2
+numTwo=3
+result = numOne & numTwo
+print(result) 
+result = numOne | numTwo 
+print(result) 
+result = ~numTwo 
+print(result) 
+result = numTwo ^ numOne 
+print(result) 
+result = numOne << numTwo 
+print(result) 
+result = numOne >> numTwo
+print(result)
