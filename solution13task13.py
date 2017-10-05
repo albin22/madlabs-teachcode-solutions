84f121de43c84e87abdd4803a61e74ea620f883b
albin22 <albinthankachan1@gmail.com>
2017-10-05 15:06:39 +0000

    13Task Submitted

---
+++ solution13task13.py
@@ -0,0 +1,13 @@
+numOne = 1 
+numOne -= 1 
+numTwo = 50 
+numTwo += 2 
+numThree = 40 
+numThree -= 2 
+numFour = 20 
+numFour *= 2 
+numFive = 80 
+numFive /= 4 
+numSix = 100 
+numSix %= 10
+print(numOne, numTwo, numThree, numFour,float(numFive), numSix)
